64cada112fed91ae64c4a5a61059fa33c4aff321
Romulo <romuabm444@gmail.com>
2025-08-27 15:05:20 -0300

    vogais

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String nome = "";
+        String[] vogais = {"a","e","i","o","u"};
+        System.out.println("Escreva um nome e eu irei mostrar quantas vogais tem: ");
+        nome = scanner.nextLine();
+        System.out.println(nome.charAt(1));
+        //for(int i = 0; i < nome.length(); i++){
+
+        }
+
+    }

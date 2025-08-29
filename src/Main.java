a89f5d73a2e6e00a6fefd2559f23caf0b0771c85
Romulo <romuabm444@gmail.com>
2025-08-29 12:01:48 -0300

    novo commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main{
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         String nome = "";
-        String[] vogais = {"a","e","i","o","u"};
+        char a = 'a';
+        int contador = 0;
         System.out.println("Escreva um nome e eu irei mostrar quantas vogais tem: ");
         nome = scanner.nextLine();
-        System.out.println(nome.charAt(1));
-        //for(int i = 0; i < nome.length(); i++){
-
+        int num = nome.length();
+        for(int i = 0; i < num; i++){
+            char letra = nome.charAt(i);
+            if (letra == a){
+                contador++;
+            }
+        } System.out.println("Esse nome tem "+contador+" vogais.");
         }
 
     }
